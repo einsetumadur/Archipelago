fdebd4de05e39d23cd726f7b8ce9461ecfab1ccf
einsetumadur <gillesoff@gmail.com>
2020-02-28 20:57:43 +0100

    rangement logistique et nouveau makefile fonctionnel. création d'un fichier source pour tout fichier de code.

--- src/rendu.cc
+++ src/rendu.cc
@@ -1,12 +1,15 @@
 /*
   ICC semestre 2 2020
-  rendu premier 
+  rendu premier
   Gilles Regamey
   Sufyan truc
 */
+
 #include<iostream>
 
 int main(){
+
+std::cout<<"hello Archipelago"<<std::endl;
 
 return 0;
 }

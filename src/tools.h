4ebf7fb61b014369f0b3f162f49be008baaea555
einsetumadur <gillesoff@gmail.com>
2020-02-29 12:11:35 +0100

    simple frist prototype of tools.h

--- src/tools.h
+++ src/tools.h
@@ -1,1 +1,15 @@
-//tools.h
+#include<iostream>
+
+#ifndef TOOLS_H
+#define TOOLS_H
+
+class Cercle { //ou structure ? a reflechir
+public:
+  Cercle();
+private:
+  double coordx,coordy;
+  double rayon;
+
+};
+
+#endif TOOLS_H

01c6a63a56f04f49e5c8416a415f24e2754dc421
einsetumadur <gillesoff@gmail.com>
2020-03-03 08:55:00 +0100

    début de structure pour Ville.cc et .h, le Makefile est réecris pour l'architecture du projet.

--- src/ville.cc
+++ src/ville.cc
@@ -1,1 +1,50 @@
-//ville.cc
+#include<iostream>
+#include<fstream>
+#include<string>
+#include<vector>
+#include"ville.h"
+
+
+Ville::Ville(string file){
+  fichier = file;
+  //besoin de construire les tableaux avant chargement ?
+  chargement(file);
+}
+
+Ville::~Ville(){
+  sauvegarde(fichier);
+}
+
+void Ville::chargement(string file){
+  fstream fichier;
+  fichier.open(file, ios::out);
+  if(!fichier.is_open()){
+    std::cout<<"impossible d'ouvrir "<<file<<std::endl;
+    //todo: mettre fonction d'erreure.
+  }else{
+    string ligne;
+    while(getline(fichier,ligne)){ //peut etre une mauvaise idée pour prendre des donées une par une.
+      std::cout<<ligne<<std::endl;
+    }
+    /*
+    while(!eof){
+    
+    }
+    */
+  }
+  string ligne;
+  fichier>>ligne
+
+  fichier.close();
+}
+
+void Ville::sauvegarde(string file){
+  fstream fichier;
+  fichier.open(file, ios::in | ios::trunc);
+
+  fichier.close();
+}
+
+void Ville::verification(){
+
+}
--- src/ville.h
+++ src/ville.h
@@ -1,1 +1,25 @@
-//ville.h
+#include<iostream>
+#include<string>
+#include<vector>
+
+#ifndef VILLE_H
+#define VILLE_H
+
+class Ville{
+  public:
+    Ville(string file);
+    ~Ville(string file);
+
+  private:
+    void chargement(string file);
+    void sauvegarde(string file);
+    void verification();
+
+    string fichier;
+    vector<Noeud_L> logement;
+    vector<Noeud_P> production;
+    vector<Noeud_T> transport;
+    vector<Lien> ponts;
+};
+
+#endif VILLE_H

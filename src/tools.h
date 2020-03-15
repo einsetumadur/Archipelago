15d26a17a231dc404b8fc00a8f24c9cd89f276ae
sufyan-zak <61593148+sufyan-zak@users.noreply.github.com>
2020-03-15 18:07:14 +0100

    Update tools.h

--- src/tools.h
+++ src/tools.h
@@ -3,20 +3,26 @@
 #ifndef TOOLS_H
 #define TOOLS_H
 
-struct Point {
-  double pos_x, pos_y;
+struct Point 
+{
+  double pos_x;
+  double pos_y;
 };
 
-struct Cercle {
+struct Cercle 
+{
   Point centre;
   double rayon;
 }; 
 
-struct Vecteur {
-  double x, y;
+struct Vecteur 
+{
+  double x;
+  double y;
 };
 
-struct Seg_droite {
+struct Seg_droite 
+{
   Point debut;
   Vecteur directeur;
 };
@@ -24,9 +30,9 @@ struct Seg_droite {
 double distance_vecteurs(Vecteur a, Vecteur b);
 bool collision_cercle(Cercle c, Cercle e, double dist_min = 0);
 double prod_scal(Vecteur a, Vecteur b);
-Vecteur projection(Vecteur a, Vecteur b);
-double dist_point_ligne(Point p, Seg_droite d);
-bool collision_droite_cercle(Cercle c, Seg_droite d, double dist_min = 0);
 double norme(Vecteur a);
+Vecteur projection(Vecteur a, Vecteur b);
+Vecteur vecteur_orthogonal(Point p, Seg_droite d);
+bool collision_droite_cercle(Cercle c, Seg_droite d, double dist_min = 0);
 
 #endif 

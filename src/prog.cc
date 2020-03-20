2dfd183edda55d41c488526afdf2a3c61917ae8b
einsetumadur <gillesoff@gmail.com>
2020-03-20 17:01:20 +0100

    ajout de prog, le fichier de main.

---
+++ src/prog.cc
@@ -0,0 +1,16 @@
+/*
+  ICC semestre 2 2020
+  rendu premier
+  Gilles Regamey
+  Sufyan Zakeeruddin
+*/
+
+#include<iostream>
+#include"ville.h"
+
+int main(int argc, char** argv){
+
+Ville Lausanne(argv[1]);
+
+return 0;
+}

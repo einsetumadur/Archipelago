5f18fa30b3185bdafcc27f0f6738eb406ba959fb
sufyan-zak <61593148+sufyan-zak@users.noreply.github.com>
2020-03-21 12:38:45 +0100

    Update noeud.h

--- src/noeud.h
+++ src/noeud.h
@@ -1,5 +1,3 @@
-
-
 #include <iostream>
 #include "tools.h"
 #include <string>
@@ -37,7 +35,7 @@ private:
 
 
 void test_lien_quartier(Noeud* A, Noeud* C, Noeud* B);	
-void test_coll_quartier(std::vector<Noeud> ensN);
+void test_coll_quartier(std::vector<Noeud*> ensN);
 void test_nbp(unsigned int nbp, double rayon);
 
 #endif

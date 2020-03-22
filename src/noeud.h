3464d71bd5b51ae414bf1c727252c8d70a838bb0
sufyan-zak <61593148+sufyan-zak@users.noreply.github.com>
2020-03-22 16:43:46 +0100

    Update noeud.h

--- src/noeud.h
+++ src/noeud.h
@@ -12,30 +12,28 @@ class Noeud
 public:
 	Noeud(unsigned int id,double x, double y, unsigned int capacite, 
 												Type_quartier type);
-	//FAIRE UN DESTRUCTEUR
 	std::string print();
-	unsigned int get_nbp();
-	double getx() const ;
-	double gety() const ;
-	double getrayon() const ;
-	Cercle getQuartier() const ;
+	unsigned int getNbp();
+	double getX() const ;
+	double getY() const ;
+	double getRayon() const ;
+	Cercle getBatiment() const ;
 	unsigned int getUid() const ;
 	Type_quartier getType() const;	
-	void add_lien(Noeud* B, std::vector<Noeud*> ensN);	
+	void add_lien(Noeud* b, std::vector<Noeud*> ensN);	
 	bool operator==(const Noeud& nd) const;
 	void test_max_link();
-	int verif_capacite(Noeud node);
 private:
-	Cercle quartier;
+	Cercle batiment;
 	unsigned int uid;
 	unsigned int nbp;
 	Type_quartier type;
 	std::vector<Noeud*> liens;
 };
 
-
-void test_lien_quartier(Noeud* A, Noeud* C, Noeud* B);	
+void test_lien_quartier(Noeud* lien_a, Noeud* lien_b, Noeud* nd);
 void test_coll_quartier(std::vector<Noeud*> ensN);
 void test_nbp(unsigned int nbp);
 
 #endif
+

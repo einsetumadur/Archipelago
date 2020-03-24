b1b5dd22db3ec33cf3a47c3041b9833703255361
sufyan-zak <61593148+sufyan-zak@users.noreply.github.com>
2020-03-24 21:04:36 +0100

    Update ville.cc

--- src/ville.cc
+++ src/ville.cc
@@ -23,7 +23,7 @@ Ville::Ville(char* file)
 	chargement(file);
 }
 
-void Ville::chargement( char * nom_fichier)
+void Ville::chargement(char* nom_fichier)
 {
 	string line;
 	ifstream fichier(nom_fichier); 
@@ -34,9 +34,9 @@ void Ville::chargement( char * nom_fichier)
 		if(line[0]=='#')  continue;  
 		decodage(line);
 		}
-	cout<<error::success()<<endl;
+	cout << error::success() << endl;
 	}
-	else cout<<"unable to open file."<<endl;
+	else cout << "unable to open file." << endl;
 }
 
 void Ville::decodage(string line)
@@ -46,79 +46,82 @@ void Ville::decodage(string line)
 	enum Etat_lecture {NBL,LOGE,NBP,PROD,NBT,TRAN,NBLI,LIENS,FIN};
 
 	static int etat(NBL); // etat de base
-	static int i(0), total(0);
-	unsigned int uid1,uid2;
+	static int i(0);
+	static int total(0);
+	unsigned int uid1;
+	unsigned int uid2;
 
 	switch(etat) 
 	{
 		case NBL: 
-			if(!(data >> total)) cout<<"wrong input format"<<endl; else i=0 ;
-			if(total==0) etat=NBT; else etat=LOGE ; 
+			if(!(data >> total)) cout << "wrong input format" << endl; else i = 0 ;
+			if(total==0) etat=NBT; else etat = LOGE ; 
 			break;
 
 		case LOGE: 	
 			ajout_noeud(data,i,LOGEMENT);
-			if(i == total) etat=NBT ;
+			if(i == total) etat = NBT ;
 			break;
 
 		case NBP: 
-			if(!(data >> total)) cout<<"wrong input format"<<endl; else i=0 ;
-			if(total==0) etat=NBLI; else etat=PROD ; 
+			if(!(data >> total)) cout << "wrong input format" << endl; else i = 0;
+			if(total == 0) etat = NBLI; else etat = PROD ; 
 			break;
 
 		case PROD: 
 			ajout_noeud(data,i,PRODUCTION);
-			if(i == total) etat=NBLI ; 
+			if(i == total) etat = NBLI ; 
 			break;
 
 		case NBT: 
-			if(!(data >> total)) cout<<"wrong input format"<<endl; else i=0;
-			if(total==0) etat=NBP; else etat=TRAN ; 
+			if(!(data >> total)) cout << "wrong input format" << endl; else i = 0;
+			if(total==0) etat=NBP; else etat = TRAN ; 
 			break;
 
 		case TRAN: 
 			ajout_noeud(data,i,TRANSPORT);
-			if(i == total) etat=NBP ; 
+			if(i == total) etat = NBP ; 
 			break;
 
 		case NBLI: 
-			if(!(data >> total)) cout<<"wrong input format"<<endl; else i=0;
-			if(total==0) etat=FIN; else etat=LIENS ; 
+			if(!(data >> total)) cout << "wrong input format" << endl; else i = 0;
+			if(total == 0) etat = FIN; else etat = LIENS ; 
 			break;
 
 		case LIENS: 
-			if( !(data >> uid1 >> uid2)) cout<<"wrong input format"<<endl; else
+			if( !(data >> uid1 >> uid2)) cout << "wrong input format" << endl; 
+			else
 			{
-				if(uid1==uid2)
-				{
-					cout<<error::self_link_node(uid1)<<endl;
-					exit(-1);
-				}
-			Noeud* n1 = trouve_lien(uid1); 
-			Noeud* n2 = trouve_lien(uid2);
-			n1->add_lien(n2,quartiers); 
-			n2->add_lien(n1,quartiers);
-			++i;
+				creation_lien(uid1, uid2);
+				++i;
 			}
-			if(i == total) etat=FIN ; 
+			if(i == total) etat = FIN ; 
 			break;
 
 		case FIN:  
 		break;
 
-		default: cout<<"defaultswitch error."<<endl;
+		default: cout << "defaultswitch error." << endl;
 		break;
 	}
 }
 
-Noeud* Ville::trouve_lien(unsigned int uid)
+void Ville::ajout_noeud(istringstream &param,int &counter,Type_quartier type)
 {
-	for(auto noeud : quartiers)
+	unsigned int numid;
+	unsigned int popmax;
+	double posx;
+	double posy;
+
+	if( !(param >> numid >> posx >> posy >> popmax))	
+		cout << "wrong input format" << endl;
+	else
 	{
-		if(uid == noeud->getUid()) return noeud;
+		quartiers.push_back(new Noeud(numid,posx,posy,popmax,type));
+		redondance_uid();
+		test_coll_quartier(quartiers);
+		++counter;
 	}
-	cout<<error::link_vacuum(uid)<<endl;
-	exit(-1);
 }
 
 void Ville::sauvegarde(string file)
@@ -126,7 +129,7 @@ void Ville::sauvegarde(string file)
 	fstream fichier;
 	fichier.open(file, ios::out | ios::trunc);
 
-	if(!fichier.is_open()) std::cout<<"unable to save file"<<file<<std::endl;
+	if(!fichier.is_open()) std::cout << "unable to save file" << file << std::endl;
 	else
 	{
 		fichier << print_type(LOGEMENT) << endl;
@@ -157,13 +160,64 @@ unsigned int Ville::nb_type(Type_quartier type)
 	return count;
 }
 
-void Ville::redondance_uid() // verifie la non-duplicite des uids
+void Ville::creation_lien(unsigned int uid_a, unsigned int uid_b) 
 {
-	size_t sizetab = quartiers.size();
-
-	for (size_t i = 0; i < sizetab ; i++) //parcours le tableau
+	if(uid_a == uid_b)
 	{
-		for (size_t j = i+1; j < sizetab ; j++) //parcour le reste du tableau
+		cout<<error::self_link_node(uid_a)<<endl;
+		exit(-1);
+	}
+	
+	Noeud* a = trouve_lien(uid_a); 
+	Noeud* b = trouve_lien(uid_b);
+	
+	if (a->multiple_link(b))
+	{
+		cout << error::multiple_same_link(uid_a, uid_b);
+		exit(-1);
+	}
+
+	if (a->test_max_link())
+	{
+		cout << error::max_link(uid_a);
+		exit(-1);
+	}
+	if (b->test_max_link())
+	{
+		cout << error::max_link(uid_b);
+		exit(-1);
+	}
+	
+	for(size_t i(0) ; i < quartiers.size() ; ++i) 
+	{
+		if(quartiers[i]->test_lien_quartier(a, b))
+		{
+			cout << error::node_link_superposition(quartiers[i]->getUid());
+			exit(-1);
+		}	
+	}
+	
+	a->ajout_lien(b); 
+	b->ajout_lien(a);
+}
+
+Noeud* Ville::trouve_lien(unsigned int uid)
+{
+	for(auto noeud : quartiers)
+	{
+		if(uid == noeud->getUid()) return noeud;
+	}
+	cout << error::link_vacuum(uid) << endl;
+	exit(-1);
+}
+
+void Ville::redondance_uid()
+{
+	size_t sizetab(quartiers.size());
+
+	for (size_t i = 0; i < sizetab ; i++) 
+	{
+		for (size_t j = i+1; j < sizetab ; j++)
 		{
 			if(quartiers[i]->getUid() == quartiers[j]->getUid())
 			{
@@ -174,21 +228,20 @@ void Ville::redondance_uid() // verifie la non-duplicite des uids
 	}
 }
 
-
-void Ville::ajout_noeud(istringstream &param,int &counter,Type_quartier type)
-{
-	unsigned int numid,popmax;
-	double posx,posy;
-
-	if( !(param >> numid >> posx >> posy >> popmax))
-		cout<<"wrong input format"<<endl;
-	else
+void Ville::test_coll_quartier(vector<Noeud*> ensN) 
+{		
+	size_t sizetab(ensN.size());
+	
+	for(size_t i(0) ; i < sizetab ; ++i) 
 	{
-		quartiers.push_back(new Noeud(numid,posx,posy,popmax,type));
-  	redondance_uid();
-  	test_coll_quartier(quartiers);
-		++counter;
+		for(size_t j(i+1) ; j < sizetab ; ++j) 
+		{
+			if(collision_cercle(ensN[i]->getBatiment(), ensN[j]->getBatiment()))
+			{
+			cout << error::node_node_superposition(ensN[i]->getUid(), 
+													ensN[j]->getUid());
+			exit(-1);
+			}
+		}
 	}
 }
-
-

7a3128ebaaeadaab96b0c354344c84685eec8e29
sufyan-zak <61593148+sufyan-zak@users.noreply.github.com>
2020-03-26 20:57:20 +0100

    Update tools.cc

--- src/tools.cc
+++ src/tools.cc
@@ -10,6 +10,39 @@
 #include "tools.h"
 #include "constantes.h"
 
+double prod_scal(Vecteur a, Vecteur b) 
+{
+	return a.x*b.x + a.y*b.y;
+}
+
+double norme(Vecteur a) 
+{
+	return sqrt(prod_scal(a,a));
+}
+
+Vecteur scalaire_vecteur(double k, Vecteur v) 
+{
+	Vecteur c = {k*v.x, k*v.y};
+		
+	return c;
+}
+
+Vecteur projection(Vecteur a, Vecteur b) 
+{
+	double k = (prod_scal(a,b)/prod_scal(b,b));
+	
+	return	scalaire_vecteur(k, b);
+}
+
+Vecteur vecteur_orthogonal(Point p, Seg_droite d) 
+{
+	Vecteur v_point_droite = { p.pos_x - d.debut.pos_x, p.pos_y - d.debut.pos_y };
+	Vecteur v_proj = projection(v_point_droite, d.directeur);
+	Vecteur v_orthog = { v_point_droite.x - v_proj.x, v_point_droite.y - v_proj.y };
+	
+	return v_orthog;
+}
+
 double distance_vecteurs(Vecteur a, Vecteur b) 
 {
     Vecteur v = {a.x-b.x, a.y-b.y};
@@ -27,33 +60,6 @@ bool collision_cercle(Cercle c, Cercle e)
     return ( dist <= (somme_rayon) );
 }
 
-double prod_scal(Vecteur a, Vecteur b) 
-{
-	return a.x*b.x + a.y*b.y;
-}
-
-double norme(Vecteur a) 
-{
-	return sqrt(prod_scal(a,a));
-}
-
-Vecteur projection(Vecteur a, Vecteur b) 
-{
-	double k = (prod_scal(a,b)/prod_scal(b,b));
-	Vecteur v_proj = { k*b.x, k*b.y};
-	
-	return v_proj;
-}
-
-Vecteur vecteur_orthogonal(Point p, Seg_droite d) 
-{
-	Vecteur v_point_droite = { p.pos_x - d.debut.pos_x, p.pos_y - d.debut.pos_y };
-	Vecteur v_proj = projection(v_point_droite, d.directeur);
-	Vecteur v_orthog = { v_point_droite.x - v_proj.x, v_point_droite.y - v_proj.y };
-	
-	return v_orthog;
-}
-
 bool collision_droite_cercle(Cercle c, Seg_droite d) 
 {
 	Vecteur v = vecteur_orthogonal(c.centre, d);
@@ -61,13 +67,6 @@ bool collision_droite_cercle(Cercle c, Seg_droite d)
 
 	return  (norme(v) <= (c.rayon) and 
 			(norme(projection(a, d.directeur)) <= norme(d.directeur)));
-}
-
-Vecteur scalaire_vecteur(int k, Vecteur v) 
-{
-	Vecteur c = {k*v.x, k*v.y};
-	
-	return c;
 }
 
 bool champ(Cercle a, Cercle b, Cercle c) 
@@ -82,5 +81,4 @@ bool champ(Cercle a, Cercle b, Cercle c)
 	
 	return (prod_scal(d.directeur, ac) >= 0 and 
 			prod_scal(bc, scalaire_vecteur(-1, d.directeur)) >= 0);
-
 }

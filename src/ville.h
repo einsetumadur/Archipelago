b38d50896f71de2a84675045461ff2f995469b6e
sufyan-zak <61593148+sufyan-zak@users.noreply.github.com>
2020-03-26 20:59:38 +0100

    Update ville.h

--- src/ville.h
+++ src/ville.h
@@ -17,23 +17,23 @@
 class Ville
 {
 public:
-	Ville(char* file);
-	void sauvegarde(std::string file);
+	void chargement(char* nom_fichier);
 
 private:
-	void chargement( char * nom_fichier);
 	void decodage(std::string line);
 	unsigned int nb_type(Type_quartier type);
 	std::string print_type(Type_quartier type);
-	void ajout_noeud(std::istringstream &param,int &counter,Type_quartier type);
+	void ajout_noeud(std::istringstream& param,int& counter,Type_quartier type);
 	void creation_lien(unsigned int uid_a, unsigned int uid_b);
+	void error_lien(Noeud* a, Noeud* b);
 	Noeud* trouve_lien(unsigned int uid); 
 	void redondance_uid();
-	void test_coll_quartier(std::vector<Noeud*> ensN);
+	void collis_noeuds();
 
 	std::string nom;
 	std::vector<Noeud*> quartiers;
 };
 
+void main_ville(char* nom_fichier);
 
 #endif

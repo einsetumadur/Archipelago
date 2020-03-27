3cd0463018a352a9193e2297c45c90258d829687
einsetumadur <gillesoff@gmail.com>
2020-03-27 16:45:46 +0100

    mis a jour compare.sh

--- src/compare.sh
+++ src/compare.sh
@@ -4,15 +4,37 @@ cp Archipelago ../Comparaison/
 cd ../Comparaison
 if [ $# != 0 ]
 then
-  echo "---test sur e$1 -----------------------------------------"
-  echo " "
-  ./demo console e$1.txt
-  ./Archipelago e$1.txt
-  echo " "
-  echo "--- fin du test -----------------------------------------"
-  echo " "
+  if [ $1 == "-t" ]
+  then
+
+    rm demo_output.txt prog_output.txt
+
+    for i in 01 02 03 04 05 06 07 08 09 10 
+    do
+      ./demo console e$i.txt >> demo_output.txt
+      ./Archipelago e$i.txt >> prog_output.txt
+    done
+
+    if cmp -s demo_output.txt prog_output.txt
+    then
+    printf "same output, Boulic is proud ! \n"
+    else
+    printf 'The file "%s" is different from "%s"\n' demo_output.txt prog_output.txt
+    diff demo_output.txt prog_output.txt
+    fi
+
+  else
+    echo "---test sur e$1 -----------------------------------------"
+    echo " "
+    ./demo console e$1.txt
+    ./Archipelago e$1.txt
+    echo " "
+    echo "--- fin du test -----------------------------------------"
+    echo " "
+  fi
+
 else
-  for i in 01 02 03 04 05 06 07 08 09 10
+  for i in 01 02 03 04 05 06 07 08 09 10 
   do
     echo "---test sur e$i -----------------------------------------"
     echo " "

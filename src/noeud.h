863ef50c85c1cc8fec90013790742eb554cccfbc
sufyan-zak <61593148+sufyan-zak@users.noreply.github.com>
2020-04-04 20:42:25 +0200

    Update noeud.h

--- src/noeud.h
+++ src/noeud.h
@@ -13,33 +13,55 @@
 #include <string>
 #include <vector>
 
-enum Type_quartier {PRODUCTION, TRANSPORT, LOGEMENT};
 class Noeud 
 {
 public:
-	Noeud(unsigned int id,double x, double y, unsigned int capacite, 
-			Type_quartier type);
-	std::string print();
+	Noeud(unsigned int id,double x, double y, unsigned int capacite);
 	unsigned int getNbp();
 	double getX() const ;
 	double getY() const ;
 	double getRayon() const ;
 	Cercle getBatiment() const ;
 	unsigned int getUid() const ;
-	Type_quartier getType() const;	
 	void test_nbp();
 	void ajout_lien(Noeud* b);
-	bool multiple_link(Noeud* b);	
-	bool test_max_link();
+	bool multiple_link(Noeud* b) ;	
+	virtual bool maxi_link();
 	bool collis_lien_quartier(Noeud* lien_a, Noeud* lien_b); 
 	bool operator==(const Noeud& nd) const;
 
-private:
+protected:
 	Cercle batiment;
 	unsigned int uid;
 	unsigned int nbp;
-	Type_quartier type;
 	std::vector<Noeud*> liens;
+};
+
+
+class Logement : public Noeud 
+{
+	
+public:
+	Logement(unsigned int id,double x, double y, unsigned int capacite);
+	virtual bool maxi_link() override;
+
+private:
+};
+
+class Transport : public Noeud
+{
+public:
+	Transport(unsigned int id,double x, double y, unsigned int capacite);
+private:
+};
+
+class Production : public Noeud
+{
+public:
+	Production(unsigned int id,double x, double y, unsigned int capacite);
+	 
+private:
+
 };
 
 #endif

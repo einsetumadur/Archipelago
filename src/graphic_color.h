2ccd0752dc9ae75aa74975d7a5af48e7a68bfeb9
sufyan-zak <61593148+sufyan-zak@users.noreply.github.com>
2020-04-25 16:44:05 +0200

    Add files via upload

---
+++ src/graphic_color.h
@@ -0,0 +1,14 @@
+/**\
+ * \name   graphics.h
+ * \author Regamey Gilles & Zakeeruddin Sufyan 
+ * \date    march 2020
+ * \version 1.0
+ */
+
+#ifndef GRAPHIC_COLOR_H
+#define GRAPHIC_COLOR_H
+
+enum Couleur {NOIR, ROUGE, VERT};
+
+#endif 
+
---
+++ src/graphic_gui.h
@@ -0,0 +1,9 @@
+#ifndef GRAPHIC_GUI_H
+#define GRAPHIC_GUI_H
+
+#include <gtkmm.h>
+#include "graphics.h"
+
+void graphic_set_context(const Cairo::RefPtr<Cairo::Context>& cr);
+
+#endif 

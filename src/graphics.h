57020f59054e77bd2b575734f2c47f00a43df6a0
sufyan-zak <61593148+sufyan-zak@users.noreply.github.com>
2020-04-25 16:42:46 +0200

    Update graphics.h

--- src/graphics.h
+++ src/graphics.h
@@ -4,3 +4,16 @@
  * \date    march 2020
  * \version 1.0
  */
+
+#ifndef GRAPHIC_H
+#define GRAPHIC_H
+
+#include "graphic_color.h"
+
+void ligne(double a_x, double a_y, double b_x, double b_y, Couleur paint);
+void cercle(double c_x, double c_y, double rayon, Couleur paint);
+void rectangle(double c_x, double c_y, double rayon, Couleur paint);
+void choix_couleur(Couleur paint);
+
+#endif 
+

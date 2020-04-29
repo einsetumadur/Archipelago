13bb26ba651a8a4212c784a9b44de4aab7023eae
sufyan-zak <61593148+sufyan-zak@users.noreply.github.com>
2020-04-29 23:47:45 +0200

    Update graphic_color.h

--- src/graphic_color.h
+++ src/graphic_color.h
@@ -10,5 +10,16 @@
 
 enum Couleur {NOIR, ROUGE, VERT};
 
-#endif 
+constexpr double epaisseur_trait(5.5);
+constexpr double epaisseur_bord(10);
+constexpr double increment_zoom(0.2);
+constexpr double zoom_initial(1.5);
+constexpr int size_graphe(800);
+constexpr int left_click(1);
+constexpr int right_click(2);
+constexpr char a('a');
+constexpr char w('w');
+constexpr char s('s');
+constexpr char d('d');
 
+#endif 

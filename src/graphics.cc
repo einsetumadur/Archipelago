841df7fa62f2feeca1d9bfaf3e1bac5f6428fbe2
sufyan-zak <61593148+sufyan-zak@users.noreply.github.com>
2020-04-29 23:48:37 +0200

    Update graphics.cc

--- src/graphics.cc
+++ src/graphics.cc
@@ -6,6 +6,7 @@
  */
 
 #include "graphic_gui.h"
+#include <iostream>
 using namespace std;
 
 static const Cairo::RefPtr<Cairo::Context>* ptcr(nullptr); 
@@ -39,18 +40,17 @@ void choix_couleur(Couleur paint)
 	}
 }
 void ligne(double a_x, double a_y, double b_x, double b_y, Couleur paint)
-{
-	(*ptcr)->set_line_width(2.0);
+{	
+	(*ptcr)->set_line_width(epaisseur_trait);
 	choix_couleur(paint);
 	(*ptcr)->move_to(a_x, a_y);
-
 	(*ptcr)->line_to(b_x,b_y);
-
 	(*ptcr)->stroke();
 }
 
 void cercle(double c_x, double c_y, double rayon, Couleur paint)
 { 
+	(*ptcr)->set_line_width(epaisseur_trait);
 	(*ptcr)->begin_new_sub_path ();
 	(*ptcr)->arc (c_x, c_y, rayon, 0, 2*M_PI);
 	(*ptcr)->set_source_rgb (1,1,1);
@@ -61,7 +61,7 @@ void cercle(double c_x, double c_y, double rayon, Couleur paint)
 
 void rectangle(double c_x, double c_y, double rayon, Couleur paint)
 {
-	(*ptcr)->set_line_width(2.0);
+	(*ptcr)->set_line_width(epaisseur_trait);
 	choix_couleur(paint);
 	(*ptcr)->move_to(c_x, c_y + rayon/ 4); //MAGIC NUMBER
 	(*ptcr)->line_to(c_x - (0.75*rayon), c_y + rayon/4);

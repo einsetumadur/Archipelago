80f232cc27f15fcca3665e2b14f4083b75c77d06
sufyan-zak <61593148+sufyan-zak@users.noreply.github.com>
2020-04-30 23:04:44 +0200

    Update ville.h

--- src/ville.h
+++ src/ville.h
@@ -1,8 +1,8 @@
 /**\
  * \name    ville.h
  * \author  Regamey Gilles & Zakeeruddin Sufyan 
- * \date    march 2020
- * \version Rendu 2
+ * \date    Mai 2020
+ * \version Rendu 2 - Architecture 11 b1
  */
 
 #ifndef VILLE_H
@@ -21,19 +21,23 @@ class Ville
 public:
 	Ville(bool val);
 	~Ville();
+	
 	double mta();
 	double enj();
 	double ci();
 	void chargement(char* nom_fichier);
-	bool get_chargement_verif();
-	unsigned int get_error_param_un();
-	unsigned int get_error_param_deux();
+	
+	bool get_chargement_verif() const { return chargement_verif; }
+	unsigned int get_error_param_un() const { return error_param_un; }
+	unsigned int get_error_param_deux() const { return error_param_deux; }
 	void reset();
+	
 	// sauvegarde du fichier :
 	void sauvegarde(std::string file) const;
 	std::string print_type(std::string type) const;
 	unsigned int nb_type(std::string type) const;
 	unsigned int nb_liens() const;
+	
 	// dessins
 	void draw_ville(Couleur paint) const;
 	void draw_liens(Couleur paint) const;
@@ -42,8 +46,8 @@ public:
 
 private:
 	void decodage(std::string line, int& etat);
-	static Ville* getVille();
-	// creation de la ville & fonctions errors
+	
+	// fonctions creation de la ville & errors
 	void ajout_noeud(std::istringstream& param,int& counter, Etat_lecture type);
 	void error_noeud(Noeud* const nd);
 	void creation_lien(unsigned int uid_a, unsigned int uid_b);
@@ -52,7 +56,6 @@ private:
 	void redondance_uid(unsigned int numid);
 	void collis_noeuds();
 
-	std::string nom;
 	bool chargement_verif;
 	Type_error msg_error;
 	unsigned int error_param_un;

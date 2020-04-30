7dde9d656529b0f29f6b88410f21199075e47d49
sufyan-zak <61593148+sufyan-zak@users.noreply.github.com>
2020-04-30 23:00:40 +0200

    Update graphic_color.h

--- src/graphic_color.h
+++ src/graphic_color.h
@@ -1,8 +1,8 @@
 /**\
- * \name   graphics.h
- * \author Regamey Gilles & Zakeeruddin Sufyan 
- * \date    march 2020
- * \version 1.0
+ * \name    graphic_color.h
+ * \author  Regamey Gilles & Zakeeruddin Sufyan 
+ * \date    Mai 2020
+ * \version Rendu 2 - Architecture 11 b1
  */
 
 #ifndef GRAPHIC_COLOR_H
@@ -12,9 +12,6 @@ enum Couleur {NOIR, ROUGE, VERT, BLANC};
 
 constexpr double epaisseur_trait(5.5);
 constexpr double epaisseur_bord(10);
-constexpr double increment_zoom(0.2);
-constexpr double zoom_initial(1.5);
-constexpr int size_graphe(800);
 constexpr int left_click(1);
 constexpr int right_click(2);
 constexpr char a('a');

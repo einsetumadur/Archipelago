0061fe2c3d6a8b8e8b98864fa4e3392784646687
sufyan-zak <61593148+sufyan-zak@users.noreply.github.com>
2020-04-30 23:01:03 +0200

    Update graphic_gui.h

--- src/graphic_gui.h
+++ src/graphic_gui.h
@@ -1,3 +1,10 @@
+/**\
+ * \name    graphic_gui.h
+ * \author  Regamey Gilles & Zakeeruddin Sufyan 
+ * \date    Mai 2020
+ * \version Rendu 2 - Architecture 11 b1
+ */
+ 
 #ifndef GRAPHIC_GUI_H
 #define GRAPHIC_GUI_H
 
